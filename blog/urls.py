6454604e0a5a5627188905605ad19c33fe5ec189
TwoUfo <94078255+TwoUfo@users.noreply.github.com>
2024-04-25 16:56:48 +0300

    Edited models, views and more

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from blog.views import *
 
 urlpatterns = [
     path('', index),
-    path("post/<slug>/", post_detail, name="blog-post-detail")
+    path("post/<slug>/", post_detail, name="blog-post-detail"),
+    path("ip/", get_ip)
 ]
+
+from django.conf import settings
+print(f"Time zone: {settings.TIME_ZONE}")
--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+import logging
+from django.views.decorators.cache import cache_page
 
+logger = logging.getLogger(__name__)
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
@@ -19,6 +24,11 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                
+                logger.info(
+                    "Created comment on Post %d for user %s", post.pk, request.user
+                )
+                
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
@@ -28,3 +38,7 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+    
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
